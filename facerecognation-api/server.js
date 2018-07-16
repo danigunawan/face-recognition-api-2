ce70b71eecf20ba09931b309fc13ee9bdc90d0b9
Shijia Hu <shijiahu2016a@gmail.com>
2018-07-15 23:47:10 -0400

    clean up the server.js to register.js

--- facerecognation-api/server.js
+++ facerecognation-api/server.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -20,9 +22,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-// app.get('/', (req, res)=>{
-// 	res.send(database.users);
-// })
+app.get('/', (req, res)=>{
+	res.send(database.users);
+})
 
 app.get('/profile/:id', (req, res) => {
 	const {id} = req.params;
@@ -60,42 +62,7 @@ app.post('/signin', (req, res)=>{
 		.catch(err => res.status(400).json('wrong credentials'));
 })
 
-app.post('/register', (req, res)=>{
-	const {email, password, name}= req.body;
-	// Store hash in your password DB.
-	const hash = bcrypt.hashSync(password);
-
-	db.transaction(trx =>{
-		trx.insert({
-			hash: hash,
-			email: email,
-		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail =>{
-			return trx('users')
-				.returning('*')
-				.insert({
-					name: name,
-					email: loginEmail[0],
-					joined: new Date()
-				})
-				.then(user => {
-					res.json(user[0]);
-				})
-		})
-		.then(trx.commit)
-		.catch(trx.rollback)
-	})
-	.catch(err => res.status(400).json('unable to sign up'));
-
-	// bcrypt.compareSync(someOtherPlaintextPassword, hash); // false
-	// bcrypt.hash(password, null, null, function(err, hash) {
-	// 	console.log(hash);
- //    // Store hash in your password DB.
-	// });
-	
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
 app.put('/image', (req, res)=>{
 	const {id} = req.body;
