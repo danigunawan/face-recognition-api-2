a2f9608beb2e1dbd4015f8a4ba9d1802ce52b3a3
Shijia Hu <shijiahu2016a@gmail.com>
2018-07-15 23:53:27 -0400

    clean up signin function

--- facerecognation-api/server.js
+++ facerecognation-api/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const knex = require('knex');
 
 const register = require('./controllers/register');
+const signin = require('./controllers/signin');
 
 const db = knex({
   client: 'pg',
@@ -40,28 +41,7 @@ app.get('/profile/:id', (req, res) => {
 	.catch(err => res.status(400).json('error getting users'));
 })
 
-app.post('/signin', (req, res)=>{
-	db.select('email', 'hash').from('login')
-		.where('email', '=', req.body.email)
-		.then(data=>{
-			// Load hash from your password DB.
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash); // true
-			console.log(isValid);
-			if (isValid) {
-				return db.select('*').from('users')
-					.where('email', '=', req.body.email)
-					.then(user =>{
-						console.log(user);
-						res.json(user[0]);
-					})
-					.catch(err => res.status(400).json('unable to get user'))
-			}else{
-				res.status(400).json('wrong credentials');
-			}
-		})
-		.catch(err => res.status(400).json('wrong credentials'));
-})
-
+app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
 app.put('/image', (req, res)=>{
