b7266add33f0e09dd8240a8a73d5a38d39a25026
Shijia Hu <shijiahu2016a@gmail.com>
2018-08-15 00:25:54 -0700

    fix database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-amorphous-19520',
-    user : '',
-    password : '',
-    database : 'face-recognition'
+    host : process.env.DATABASE_URL,
+  	ssl: true,
   }
 });
 
