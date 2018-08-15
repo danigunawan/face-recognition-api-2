0791911cb14f435c515b9838bcd1d5c7cb66ed57
Shijia Hu <shijiahu2016a@gmail.com>
2018-08-15 15:21:59 -0700

    back

--- controllers/image.js
+++ controllers/image.js
@@ -21,7 +21,7 @@ const handleApiCall = (req,res) =>{
 const handleImage = (req, res, db)=>{
 	const {id} = req.body;
 	db('users').where('id', '=', id)
-	// .increment('entries',1)
+	.increment('entries',1)
 	.returning('entries')
 	.then(entries =>{
 		res.json(entries[0]); 
